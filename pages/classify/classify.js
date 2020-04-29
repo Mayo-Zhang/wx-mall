a72e567592116c522e44fb8a979c4c1498242a2d
zhangmiao <mayo_zhang@126.com>
2020-04-29 23:45:43 +0800

    修改npm包路径，增加页面

--- pages/classify/classify.js
+++ pages/classify/classify.js
@@ -62,5 +62,21 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  onClose(event) {
+    const { position, instance } = event.detail;
+    switch (position) {
+      case 'left':
+      case 'cell':
+        instance.close();
+        break;
+      case 'right':
+        Dialog.confirm({
+          message: '确定删除吗？'
+        }).then(() => {
+          instance.close();
+        });
+        break;
+    }
   }
 })
